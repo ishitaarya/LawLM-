5adaf1359e536d2cf18677e0bbbdec392fe5242f
ishitaarya <ishitaarya230640@acropolis.in>
2026-09-23 17:28:21 +0530

    Add evaluation utility test

---
+++ src/evaluation/test_evaluation.py
@@ -0,0 +1,10 @@
+import math
+
+assert math.exp(0.0) == 1.0
+assert math.isclose(math.exp(1.0), math.e)
+
+print("=" * 60)
+print("LawLM - Evaluation Utility Test")
+print("=" * 60)
+print("Perplexity calculation test passed!")
+print("=" * 60)
